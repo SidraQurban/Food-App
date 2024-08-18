0f3c442594ca5f6b6c7508495106545789b7ef59
SidraQurban <sidraqurban77@gmail.com>
2024-08-18 22:55:46 +0500

    updated

--- app/components/Payment.js
+++ app/components/Payment.js
@@ -1,7 +1,10 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const Payment = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <View
@@ -234,6 +237,7 @@ const Payment = () => {
           }}
         />
         <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
           style={{
             marginLeft: 11,
             width: 370,
