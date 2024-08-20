aef8f86a649d5a8b9de9c5a3b439f7bf9e59288a
SidraQurban <sidraqurban77@gmail.com>
2024-08-20 06:05:19 +0500

    .

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,10 +1,10 @@
-import { View, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
 const Card = () => {
   return (
     <View>
-      <TouchableOpacity
+      <View
         style={{
           marginTop: 20,
           marginLeft: 10,
@@ -18,7 +18,7 @@ const Card = () => {
           source={require("../../assets/deal.png")}
           style={{ resizeMode: "cover", height: 180, width: "100%" }}
         />
-      </TouchableOpacity>
+      </View>
     </View>
   );
 };
